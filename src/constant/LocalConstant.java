a2ffdc37a64fa189066b3dc9fc5f30865ba4aee5
masashi <masashi_69@live.jp>
2017-11-23 00:18:57 +0900

    コンパイルを通るようにした
    多分windowsだと動くはず

---
+++ src/constant/LocalConstant.java
@@ -0,0 +1,7 @@
+package constant;
+
+public interface LocalConstant {
+    int COMMENT_NUMBER_DEFAULT = 0;
+    String LIVETUBE_URL = "http://livetube.cc/%s";
+    String LIVETUBE_URL_JSON = "http://livetube.cc/index.live.json";
+}
